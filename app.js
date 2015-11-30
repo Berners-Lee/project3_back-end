27aac044762bd4d8d51c2343db4080c0965b2961
gotwilk890 <dave.wilk890@gmail.com>
2015-11-30 13:57:25 -0500

    Adds a controller and routes for Order model.
    Adds a root route for orders and connects to app.js also connects model to database
    
    DW

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var passport = require('./lib/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var orders = require('./routes/orders');
 
 var app = express();
 
@@ -53,6 +54,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/orders', orders);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/orders.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var orders = require('../controllers/orders');
+
+/* GET home page. */
+router.get('/', orders.root.get);
+
+/**AUTH ROUTES
+ *  a login route using `passport.authenticate`
+ *  a register route **not using passport**
+ *
+ */
+// router.route('/login').
+//     get(authCtrl.deny).
+//     post(authCtrl.login.post).
+//     all(authCtrl.login.all);
+
+// router.route('/logout').
+//   all(authCtrl.logout.all);
+
+// router.route('/changePassword').
+//   get(authCtrl.deny).
+//   patch(authCtrl.changePassword.patch);
+
+// router.route('/signup').
+//   get(authCtrl.deny).
+//   post(authCtrl.signup.post);
+
+// router.route('/destroy').
+//   delete(authCtrl.destroy.delete);
+
+module.exports = router;
