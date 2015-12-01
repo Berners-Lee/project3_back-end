da0f4ef9d10438812b2c0ec71af336a0d4e0abc9
gotwilk890 <dave.wilk890@gmail.com>
2015-12-01 13:31:58 -0500

    Adds add cart and delete cart methods to controller/profiles.js
    Both methods add product id to session.cart and pushes it to profile.cart and saves it.

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -38,14 +38,27 @@ module.exports = {
             });
         }
     },
-    update : {
+    addCart : {
         patch : function(req, res, next) {
             Profile.find({user_ObjectId: req.user._id}).exec().then(function(profile){
-                profile[0].currentCart = req.body.currentCart;
-                profile[0].shoppingHistory = req.body.shoppingHistory;
+                if (!req.session.cart) req.session.cart = [];
+                req.session.cart.push(req.body.temp);
+                profile[0].cart.push(req.body.temp);
                 profile[0].save(function(err){
                     if (err) return next(err);
-                    res.send('updated');
+                    res.send('added');
+                });
+            });
+        }
+    },
+    deleteCart : {
+        patch : function(req, res, next) {
+            Profile.find({user_ObjectId: req.user._id}).exec().then(function(profile){
+                req.session.cart.splice(req.session.cart.indexOf(req.body.temp),1);
+                profile[0].cart.splice(profile[0].cart.indexOf(req.body.temp),1);
+                profile[0].save(function(err){
+                    if (err) return next(err);
+                    res.send('deleted');
                 });
             });
         }
