47ba64eb7a7799dd436ffbc1fcdd0833f82c45a3
gotwilk890 <dave.wilk890@gmail.com>
2015-12-01 13:44:52 -0500

    Adds proper routing for new profile methods
    DW

--- routes/profiles.js
+++ routes/profiles.js
@@ -5,7 +5,8 @@ var profiles = require('../controllers/profiles');
 /* GET home page. */
 router.get('/', profiles.root.get);
 router.post('/', profiles.create.post);
-router.patch('/', profiles.update.patch);
+router.patch('/add', profiles.addCart.patch);
+router.patch('/delete', profiles.deleteCart.patch);
 router.delete('/', profiles.destroy.delete);
 
 /**AUTH ROUTES
