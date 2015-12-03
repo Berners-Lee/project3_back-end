a5d9c2f95bd3fa3165b4d712e1b70e77071e5045
kuramameng <mengjs324@gmail.com>
2015-12-02 21:30:37 -0500

    Adds res.redirect('back') to routes/charges.js
    After every payment, user will be redirected to homepage
    
    jsm

--- routes/charges.js
+++ routes/charges.js
@@ -6,11 +6,11 @@ router.post('/', function(req, res, next){
   stripe.charges.create({
     amount: 2000,
     currency: 'usd',
-    source: req.body.stripeToken,
+    source: req.body.stripeToken
   }, function(err, charge){
     if(err) return next(err);
-    console.log(charge);
-    res.json();
+    console.log("charged");
+    res.redirect('back');
   });
 });
 
