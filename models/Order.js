5734ae1d349b925e2545c5edf02511f462cb5415
gotwilk890 <dave.wilk890@gmail.com>
2015-12-03 11:55:48 -0500

    Adds a date time stamp to orders.
    This way the orders can be viewed with date purchased.
    
    Dw

--- models/Order.js
+++ models/Order.js
@@ -2,6 +2,24 @@
 
 var mongoose = require('mongoose');
 
+var todaysDate = function(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    if(mm<10) {
+      mm='0'+mm
+    }
+
+    today = mm+'/'+dd+'/'+yyyy;
+    return today;
+  };
+
 var orderSchema = new mongoose.Schema({
   product_ObjectId: {
     type: [mongoose.Schema.Types.ObjectId],
@@ -12,6 +30,12 @@ var orderSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: "Profile"
+  },
+  date: {
+    type: 'String',
+    default: todaysDate(),
+    required: true,
+    ref: 'Date Purchased'
   }
 });
 
